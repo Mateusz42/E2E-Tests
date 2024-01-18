97eb3b73a88d740f426916437ff6c9093b5dec4b
Mateusz <135542253+Mateusz42@users.noreply.github.com>
2024-01-18 11:56:58 +0100

    Delete cypress/e2e/goit.cy.js

--- cypress/e2e/goit.cy.js
+++
@@ -1,8 +0,0 @@
-describe('User can visit to GoIT page' , () => {
-   it('Successfully navigates to The GoIt page' , () => {
-    cy.visit('https://www.edu.goit.global/account/login');
-    
-    cy.url().should('include' , 'account/login')
-   
-   });
-});
